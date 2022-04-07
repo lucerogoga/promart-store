24b3fb6a37a28e30ee076d7a7224f571ab3587a0
Lucero <lucerogoga63@hotmail.com>
2022-04-06 22:02:08 -0500

    add services from the api

---
+++ promart-store/src/services/apiServices.js
@@ -0,0 +1,31 @@
+export function saveToken(token) {
+  localStorage.setItem("token", token);
+}
+
+export const login = (userName, password) => {
+  return fetch("https://fakestoreapi.com/auth/login", {
+    // mode: "no-cors",
+    method: "POST",
+    body: JSON.stringify({
+      username: "johnd",
+      password: "m38rmF$",
+    }),
+  })
+    .then((res) => res.json())
+    .then((json) => console.log(json))
+    .catch((error) => console.log("Authorization failed : " + error.message));
+};
+
+export const getCategories = () => {
+  return fetch("https://fakestoreapi.com/products/categories");
+};
+
+export const getAllProducts = () => {
+  return fetch("https://fakestoreapi.com/products").then((res) => res.json());
+};
+
+export const filterProductByCategory = (catName) => {
+  return fetch(`https://fakestoreapi.com/products/category/${catName}`).then(
+    (res) => res.json()
+  );
+};

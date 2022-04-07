85bf0a3b764404210099582228c947f180b755ab
Lucero <lucerogoga63@hotmail.com>
2022-04-07 11:16:29 -0500

    add button to clean filter and css

--- promart-store/src/components/TakeOrder/index.js
+++ promart-store/src/components/TakeOrder/index.js
@@ -6,7 +6,8 @@ import { v4 as uuidv4 } from "uuid";
 //Components
 import iconCategories from "components/IconCategories";
 import ButtonFilter from "components/ButtonFilter";
-import ProductCard from "components/ProductCard";
+import ProductList from "components/ProductList";
+import { ReactComponent as Trash } from "../../icons/trash.svg";
 //CustomHooks
 import { useCategories } from "features/product/hooks/useCategories";
 import { useFilteredProducts } from "features/product/hooks/useFilteredProducts";
@@ -15,13 +16,16 @@ function TakeOrder() {
   const categories = useCategories();
   const [selectedCategory, setSelectedCategory] = useState("");
   const products = useFilteredProducts(selectedCategory);
-
   const handleClick = (catName) => {
     setSelectedCategory(catName);
   };
-
+  const cleanInputs = () => {
+    setSelectedCategory("");
+  };
   return (
     <>
+      <h1 className="title-customer">HELLO </h1>
+      <p className="sub-title-customer">Let's gets somethings?</p>
       <div className="categories-container">
         {categories.map((cat, i) => {
           return (
@@ -37,9 +41,15 @@ function TakeOrder() {
           );
         })}
       </div>
+      <div className="clean-filter-container">
+        <div className="trash-button" onClick={cleanInputs}>
+          <p>Clean Filter</p>
+          <Trash className="trash-button__icon" />
+        </div>
+      </div>
       <div className="products-container">
         {products.map((product) => {
-          return <ProductCard product={product} key={uuidv4()} />;
+          return <ProductList product={product} key={uuidv4()} />;
         })}
       </div>
     </>

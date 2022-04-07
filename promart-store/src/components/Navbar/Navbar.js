27c91da2cc4a4134617f28a8d22c684181b08261
Lucero <lucerogoga63@hotmail.com>
2022-04-07 15:01:50 -0500

    creates Navbar

---
+++ promart-store/src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { BsFillCartFill } from "react-icons/bs";
+import { AiFillHome } from "react-icons/ai";
+export const Navbar = ({}) => {
+  return (
+    <>
+      <div className="navbar-container">
+        <Link to={"/dashboard"} className="logo-container">
+          <AiFillHome /> <p>Home</p>
+        </Link>
+        <Link to={"cart"} className="logo-container">
+          <BsFillCartFill /> <p>Cart</p>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;

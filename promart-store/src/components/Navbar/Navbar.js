a23f634ef8f34fc6a1be6f0f757137b5ebbb6e18
Lucero <lucerogoga63@hotmail.com>
2022-04-07 17:48:55 -0500

    add conditions for classes in links

--- promart-store/src/components/Navbar/Navbar.js
+++ promart-store/src/components/Navbar/Navbar.js
@@ -1,17 +1,34 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { BsFillCartFill } from "react-icons/bs";
 import { AiFillHome } from "react-icons/ai";
-export const Navbar = ({}) => {
+export const Navbar = () => {
+  const location = useLocation();
+  const { pathname } = location;
   return (
     <>
       <div className="navbar-container">
-        <Link to={"/dashboard"} className="logo-container">
+        <Link
+          to={"/dashboard"}
+          className={
+            pathname === "/dashboard"
+              ? "logo-container active"
+              : "logo-container"
+          }
+        >
           <AiFillHome /> <p>Home</p>
         </Link>
-        <Link to={"cart"} className="logo-container">
-          <BsFillCartFill /> <p>Cart</p>
+        <Link
+          to={"cart"}
+          className={
+            pathname === "/dashboard/cart"
+              ? "logo-container active"
+              : "logo-container"
+          }
+        >
+          <BsFillCartFill />
         </Link>
       </div>
     </>

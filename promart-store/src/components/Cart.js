0ba067797fe2a12109038bcc95408ca19bf49653
Lucero <lucerogoga63@hotmail.com>
2022-04-07 11:19:13 -0500

    render products in MyCart

--- promart-store/src/components/Cart.js
+++ promart-store/src/components/Cart.js
@@ -1,5 +1,30 @@
+import React, { useState } from "react";
+//Libraries
+import { v4 as uuidv4 } from "uuid";
+//Redux
+import { useSelector, useDispatch } from "react-redux";
+import {
+  remove,
+  add,
+  selectProducts,
+  selectStatus,
+} from "../features/cart/cartSlice";
+//Components
+import ProductCart from "components/ProductCart";
+
 function Cart() {
-  return <></>;
+  const cart = useSelector(selectProducts);
+  // const status = useSelector(selectStatus);
+  return (
+    <>
+      <h1 className="title-cart">My Cart</h1>
+      <div className="products-container">
+        {cart.map((cartProduct) => {
+          return <ProductCart product={cartProduct} key={uuidv4()} />;
+        })}
+      </div>
+    </>
+  );
 }
 
 export default Cart;

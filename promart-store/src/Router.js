1b3b024b875c859abeaa09ca0b337c350b6350ca
Lucero <lucerogoga63@hotmail.com>
2022-04-07 17:50:10 -0500

    clean component app default

--- promart-store/src/Router.js
+++ promart-store/src/Router.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import App from "components/App";
 import Login from "pages/Login";
 import Dashboard from "pages/Dashboard";
 import TakeOrder from "components/TakeOrder";
@@ -9,7 +8,6 @@ import Cart from "components/MyCart/Cart";
 function Router() {
   return (
     <Routes>
-      <Route path="/app" element={<App />} />
       <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />}>
         {/* ANIDADO */}
